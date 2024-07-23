812e8a12feca4e6abee01c768b729e5af3b4ad03
Thaynan-Ferreira <thaynan_ferreira@outlook.com.br>
2024-07-23 15:57:43 -0300

    Levando as informações pro JS

---
+++ js/app.js
@@ -0,0 +1,8 @@
+function alterarStatus(ref) {
+    let referencial = document.querySelector(`#game-${ref}`);
+    let imagem = referencial.querySelector('.dashboard__item__img');
+    let btn = referencial.querySelector('.dashboard__item__button');
+
+    
+
+}
