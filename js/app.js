6dcbf49e95be3d755ba0485536c4e84df472a4d7
Thaynan-Ferreira <thaynan_ferreira@outlook.com.br>
2024-07-23 16:01:29 -0300

    Adicionando os cometarios ao codigo

--- js/app.js
+++ js/app.js
@@ -1,8 +1,11 @@
+//Função responsavel por realizar as trocas de estado dentro do site
 function alterarStatus(ref) {
+    //Declaração das variaveis para manipular o HTML
     let referencial = document.querySelector(`#game-${ref}`);
     let imagem = referencial.querySelector('.dashboard__item__img');
     let btn = referencial.querySelector('.dashboard__item__button');
 
+    //Condicional que verifica se o jogo está ou não alugado
     if (btn.textContent == 'Alugar'){
         imagem.classList.add('dashboard__item__img--rented');
         btn.classList.add('dashboard__item__button--return');
