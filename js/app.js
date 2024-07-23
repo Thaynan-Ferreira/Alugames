fc7c2f70d9fd30b11b51eab8708247c49c450176
Thaynan-Ferreira <thaynan_ferreira@outlook.com.br>
2024-07-23 15:58:11 -0300

    Criando a alteração de Alugar para devolver

--- js/app.js
+++ js/app.js
@@ -3,6 +3,10 @@ function alterarStatus(ref) {
     let imagem = referencial.querySelector('.dashboard__item__img');
     let btn = referencial.querySelector('.dashboard__item__button');
 
-    
+    if (btn.textContent == 'Alugar'){
+        imagem.classList.add('dashboard__item__img--rented');
+        btn.classList.add('dashboard__item__button--return');
+        btn.textContent = 'Devolver';
+    }
 
 }
